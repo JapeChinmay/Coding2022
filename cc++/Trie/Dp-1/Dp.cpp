9dcfdaead2c29c004c277e72c9c714a108ff1c3f
Chinmay Jape <iam95041@gmail.com>
2022-11-15 14:26:36 +0530

    Trie and dp

--- cc++/Trie/Dp-1/Dp.cpp
+++ cc++/Trie/Dp-1/Dp.cpp
@@ -17,7 +17,7 @@ int fibDp(int n, vector<int>&dp){
        int b = fibDp(n-2, dp);
        dp[n] = a+ b;
        return dp[n];
-       
+
     
 }
 
@@ -26,6 +26,22 @@ int main() {
        int n;
        cin >> n;
        vector<int> dp(n+1, -1);
-       cout << fibDp(n, dp);
+      // cout << fibDp(n, dp);
+
+
+       int prev = 1;
+       int prev2 = 0;
+       for(int i=2;i<=n; i++) {
+           int curr  = prev + prev2;
+           prev2 = prev;
+           prev = curr;
+
+       }
+
+       cout << prev ;
+       return 0;
+
+
+
 
 }
