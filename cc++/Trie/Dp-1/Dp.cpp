4242358be38f630f53801bffb05384a001304b3b
Chinmay Jape <iam95041@gmail.com>
2022-11-15 15:01:47 +0530

    Minsteps to 1 problem added in dp.cpp

--- cc++/Trie/Dp-1/Dp.cpp
+++ cc++/Trie/Dp-1/Dp.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 
+  // sc-> creatign a vector to maintain the ans
 
 int fibDp(int n, vector<int>&dp){
        if(n <= 1) {
@@ -21,6 +22,73 @@ int fibDp(int n, vector<int>&dp){
     
 }
 
+int minSteps(int n)  {
+       if(n <=1) {
+           return 0;
+
+       }
+
+       int x = minSteps(n-1);
+       int y=  INT_MAX;
+       int z = INT_MAX;
+
+
+       if(n%2 == 0) {
+             y=minSteps(n/2);
+       }
+
+       if(n%3==0) {
+             z = minSteps(n/3);
+
+       }
+
+       int ans =  min(x, min(y,z)) + 1;
+       return ans;
+
+
+}
+
+int minStepsDP(int n , vector<int>&dp){
+       if(n<=1) {
+         return 0;
+
+       }
+
+       if(dp[n] != -1) {return dp[n];}
+
+       ////
+
+
+       int x= minStepsDP(n-1, dp);
+       int y= INT_MAX;
+       int z = INT_MAX;
+
+
+       if(n%2 == 0) {
+            y =  minStepsDP(n/2, dp);
+            
+
+       }
+
+       if(n%3 == 0 ) {
+           z = minStepsDP(n/3, dp);
+       
+
+       }
+
+       int ans = min(x,min(y,z)) + 1;
+       
+       dp[n] = ans;
+       return ans;
+
+
+
+
+
+
+
+}
+
 int main() {
        
        int n;
@@ -28,18 +96,23 @@ int main() {
        vector<int> dp(n+1, -1);
       // cout << fibDp(n, dp);
 
+      cout << minStepsDP(n, dp);
 
-       int prev = 1;
-       int prev2 = 0;
-       for(int i=2;i<=n; i++) {
-           int curr  = prev + prev2;
-           prev2 = prev;
-           prev = curr;
 
-       }
 
-       cout << prev ;
-       return 0;
+
+//    // tabulation-> taking less space to perform 
+//        int prev = 1;
+//        int prev2 = 0;
+//        for(int i=2;i<=n; i++) {
+//            int curr  = prev + prev2;
+//            prev2 = prev;
+//            prev = curr;
+
+//        }
+
+//        cout << prev ;
+//        return 0;
 
 
 
