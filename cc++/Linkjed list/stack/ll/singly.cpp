f655b62ca2bc3b9677f535766b0665e9ba7c2297
Chinmay Jape <iam95041@gmail.com>
2022-11-16 14:37:27 +0530

    All type of insertion added to LL

--- cc++/Linkjed list/stack/ll/singly.cpp
+++ cc++/Linkjed list/stack/ll/singly.cpp
@@ -37,6 +37,47 @@ void printLL(Node* &head) {
       
 }
 
+void insertAttail( Node* &tail , int data) {
+       
+         Node*temp = new Node(data);
+         tail->next = temp;
+         tail = temp;
+
+
+
+}
+
+void insertAtPos(int pos, int data , Node* &head, Node* &tail) {
+
+        if(pos == 1) {
+             insert(head, data);
+             return;
+
+        }
+        Node* temp = new Node(data);
+        Node* start = head;
+        int c=  1;
+        while(c <pos-1) {
+               start = start->next;
+               c++;
+        }
+        if(start->next == NULL) {
+              insertAttail(tail, data);
+              return;
+
+        }
+
+        temp->next=  start->next;
+        start->next = temp;
+
+
+
+
+
+        
+
+}
+
 
 
 
@@ -44,10 +85,20 @@ int main() {
 
     Node* node1 = new Node(12);
     Node *head = node1;
-    insert(head, 12);
-    insert(head ,1212);
-    printLL(head);
-    
+    Node *tail = node1;
+
+   insertAttail(tail, 1212);
+   insertAttail(tail,10);
+   insertAtPos(4,56,head, tail);
+   
+   insertAtPos(3,33,head, tail);
+
+   printLL(head);
+
+   cout << head->data << tail->data << endl;
+
+
+
 
 
 
