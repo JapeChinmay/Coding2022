44463bb1574a6ed6c7d64d527280f9ecfcb1d10c
Chinmay Jape <iam95041@gmail.com>
2022-12-10 16:23:24 +0530

    leetcode

---
+++ cc++/Linkjed list/Binary Tree/SDE SHEET/maximum sumofsubaarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int sum = 0;
+        int ans = INT_MIN;
+
+
+        for(auto i: nums) {
+             
+              sum  += i;
+              ans = max(sum, ans);
+
+
+              if(sum < 0) {
+
+                     sum = 0;
+
+              }
+
+        }
+
+
+        return ans;
+        
+    }
+};
